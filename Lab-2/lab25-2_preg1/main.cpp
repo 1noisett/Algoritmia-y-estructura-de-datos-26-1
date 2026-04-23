0e8b54ac8b3651f107915af004780d10a79cdd2a
Miguel <noisetgoud@gmail.com>
2026-04-23 16:15:35 -0500

    resolucion completa del lab2_25-2 preg1 de listas

--- Lab-2/lab25-2_preg1/main.cpp
+++ Lab-2/lab25-2_preg1/main.cpp
@@ -17,13 +17,50 @@ using namespace std;
 99 Kevin Portero
  */
 
+void reordenaFormacion(Lista &listaTAD,string formacion[],int cantidadPosiciones){
+    NodoLista *nuevaCabeza = nullptr;
+    NodoLista *nuevaCola = nullptr;
+    // Iteramos sobre las posiciones que pide el entrenador
+    for (int p = 0; p < cantidadPosiciones; ++p) {
+        NodoLista *actual = listaTAD.inicio;
+        NodoLista *anterior = nullptr;
+        // Recorremos los jugadores de la lista original
+        while (actual) {
+            if (actual->elemento.posicion == formacion[p]) {
+                NodoLista *nodoSig = actual->siguiente;
+                if (anterior==nullptr) {
+                    listaTAD.inicio = actual->siguiente;
+                }else {
+                    anterior->siguiente = actual->siguiente;
+                }
+                // Convertimos actual a un nodo independiente
+                actual->siguiente = nullptr;
+                if (nuevaCabeza==nullptr) {
+                    nuevaCabeza = actual;
+                    nuevaCola = actual;
+                }else {
+                    nuevaCola->siguiente = actual;
+                    nuevaCola = actual;
+                }
+                // Avanzamos usando la copia de seguridad
+                actual = nodoSig;
+            }else {
+                anterior = actual;
+                actual = actual->siguiente;
+            }
+        }
+    }
+    // El paso final
+    listaTAD.inicio = nuevaCabeza;
+}
+
 int main() {
     Lista listaTAD;
     construirLista(listaTAD);
     //Formación de los  jugadores
-    string formacion[] = {"Defensa","Portero","Delantero","Mediocampo"};
+    string formacion[] = {"Defensa","Portero","Delantero","Mediocampo","Carrilero"};
     int cantidadPosiciones = size(formacion);
-
+    //Elemento jugador
     int numCamiseta;
     string nombre;
     string posicion;
@@ -32,17 +69,24 @@ int main() {
     while (input >> jugador.numCamiseta >> jugador.nombre >> jugador.posicion) {
         insertarFinal(listaTAD, jugador);
     }
-
+    //Imprimir lista jugadores
     imprimirLista(listaTAD);
-    cout<<endl<<endl<<endl;
+    cout<<"\n\n\n";
+    //Reordenar los jugadores
+    reordenaFormacion(listaTAD,formacion,cantidadPosiciones);
+    //Imprimir la lista ordenada de jugadores
+    imprimirLista(listaTAD);
 
+    return 0;
+}
+
+/*
     NodoLista *ptr = listaTAD.inicio;
     int cantidadJugadores = 0;
     while (ptr) {
         cantidadJugadores++;
         ptr = ptr->siguiente;
     }
-
     int j = 0;
     NodoLista *actual;
     NodoLista *anterior;
@@ -78,7 +122,4 @@ int main() {
         }
         j++;
     }
-    imprimirLista(listaTAD);
-
-    return 0;
-}
+ */
