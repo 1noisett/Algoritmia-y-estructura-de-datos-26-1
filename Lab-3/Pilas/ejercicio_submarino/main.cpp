1628b48b3c04e5f0ff90b611ebe76113ecd604a1
Miguel <noisetgoud@gmail.com>
2026-05-05 16:23:41 -0500

    problema del submarino ultimos detalles

--- Lab-3/Pilas/ejercicio_submarino/main.cpp
+++ Lab-3/Pilas/ejercicio_submarino/main.cpp
@@ -5,9 +5,23 @@ using namespace std;
 
 void submarino(Pila &pila,char orden[],int n) {
     //definir el nivel de inicio
-    int nivel = 1;
-    cout<<"La respuesta sería :";
+    int nivel = 2;
+    cout<<"La respuesta sería : ";
+    if (esPilaVacia(pila)) apilar(pila,{1});
+    for (int i = 0; i < n; i++) {
+        if (orden[i]=='S') {
+            while (!esPilaVacia(pila)) {
+                cout<<desapilar(pila).dato<<" ";
+            }
 
+        }
+        // Sin importar si fue 'S' o 'B', el siguiente nivel SIEMPRE se apila
+        apilar(pila,{nivel});
+        nivel++;
+    }
+    while (!esPilaVacia(pila)) {
+        cout<<desapilar(pila).dato<< " ";
+    }
 }
 //Hanoi mueve n elementos de una pila a otra, respetando el orden, usando una estructura recursiva y una pila auxiliar
 // 1 2 3
@@ -16,7 +30,9 @@ int main() {
     Pila pila;
     construirPila(pila);
 
-    char orden[] = {'B','B','S'};
+    // char orden[] = {'B','B','S'};
+    // int n = size(orden);
+    char orden[] = {'S','S','S','B','S'};
     int n = size(orden);
 
     submarino(pila,orden,n);
