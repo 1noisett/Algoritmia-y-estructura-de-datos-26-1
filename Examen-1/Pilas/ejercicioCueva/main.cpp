c8db024d8bf7a135518219f01cefc74721a3cbc3
Miguel <noisetgoud@gmail.com>
2026-05-19 23:46:51 -0500

    ejercicio cueva pilas ex1(25-1)

---
+++ Examen-1/Pilas/ejercicioCueva/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include "BibliotecaPilas/funcionesPila.h"
+#include "BibliotecaPilas/Pila.h"
+using namespace std;
+
+int main() {
+    Pila pila;
+    construirPila(pila);
+
+    return 0;
+}
