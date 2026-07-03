39de75fbb49f6d64fddc7045a5c8eafdc1941362
Miguel <noisetgoud@gmail.com>
2026-07-03 11:42:33 -0500

    rotaciones avl

---
+++ Lab-5/ABB/rotacionesAVL/BibliotecaArbolBinarioBusqueda/ElementoArbolBinarioBusqueda.h
@@ -0,0 +1,12 @@
+//
+// Created by Miguel Bazan on 23/06/26.
+//
+
+#ifndef FUNCIONESABB_ELEMENTOARBOLBINARIOBUSQUEDA_H
+#define FUNCIONESABB_ELEMENTOARBOLBINARIOBUSQUEDA_H
+
+struct ElementoArbolBinarioBusqueda {
+    int numero;
+};
+
+#endif //FUNCIONESABB_ELEMENTOARBOLBINARIOBUSQUEDA_H
