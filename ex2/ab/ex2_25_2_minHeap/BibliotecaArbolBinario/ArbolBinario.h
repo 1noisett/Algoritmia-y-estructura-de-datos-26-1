f90e57640a1b300ee53aa6930bb66d92bb683a97
Miguel <noisetgoud@gmail.com>
2026-07-10 16:03:54 -0500

    ex2 ab min heap

---
+++ ex2/ab/ex2_25_2_minHeap/BibliotecaArbolBinario/ArbolBinario.h
@@ -0,0 +1,13 @@
+//
+// Created by Miguel Bazan on 10/06/26.
+//
+
+#ifndef FUNCIONESARBOLES_ARBOLBINARIO_H
+#define FUNCIONESARBOLES_ARBOLBINARIO_H
+#include "NodoArbolBinario.h"
+
+struct ArbolBinario {
+    NodoArbolBinario *raiz;
+};
+
+#endif //FUNCIONESARBOLES_ARBOLBINARIO_H
---
+++ ex2/ab/ex2_25_2_minHeap/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include "BibliotecaArbolBinario/ArbolBinario.h"
+#include "BibliotecaArbolBinario/FuncionesArbolBinario.h"
+using namespace std;
+
+int main() {
+    ArbolBinario arbol;
+    construir(arbol);
+
+    return 0;
+}
